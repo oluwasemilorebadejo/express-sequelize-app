cddda3dcf04c59b407699386f86f0358d9553222
blac <badejo.semilore.7@gmail.com>
2023-02-15 12:19:09 +0100

    added search

--- routes/gigs.js
+++ routes/gigs.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Gig = require("../models/gigModel");
+const { Op } = require("sequelize");
 
 router.get("/", async (req, res) => {
   const gigs = await Gig.findAll();
@@ -28,6 +29,21 @@ router.post("/", async (req, res) => {
     status: "success",
     data: {
       gig,
+    },
+  });
+});
+
+router.get("/search", async (req, res) => {
+  const { technologies } = req.query;
+
+  const gigs = await Gig.findAll({
+    where: { technologies: { [Op.like]: `%${technologies}%` } },
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      gigs,
     },
   });
 });
